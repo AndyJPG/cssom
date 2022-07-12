3235556ae02adf1dc3d69d80fdd3e50a0ea9c623
peigengjiang <peigeng.jiang@preezie.com>
2022-07-12 21:40:02 +1000

    added simple widget

---
+++ src/components/Widget.tsx
@@ -0,0 +1,19 @@
+const Widget = () => {
+  return (
+    <div className="prz-widget-container">
+      <h1 className="prz-widget-h1 prz-widget-h1-c123">Start your journey</h1>
+      <h4 className="prz-widget-h4 prz-widget-h4-c123">Find out how global brands have optimised their customer experience</h4>
+      <p className="prz-widget-p prz-widget-p-c123">What would you like to do better to accelerate your growth?</p>
+      <p className="prz-widget-p prz-widget-p-c456">You can choose more than one!</p>
+      <div className="prz-widget-answer-wrapper prz-widget-answer-wrapper-c123">
+        answer place holder
+      </div>
+      <div className="prz-widget-button-wrapper prz-widget-button-wrapper-c123">
+        <button className="prz-widget-button prz-widget-button-c123">back</button>
+        <button className="prz-widget-button prz-widget-button-c456">next</button>
+      </div>
+    </div>
+  )
+}
+
+export default Widget
